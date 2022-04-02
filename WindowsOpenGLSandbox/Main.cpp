11a83bcf2498debcdee212535265ed636aab80f8
Whit Jackman <whitjackman@gmail.com>
2022-04-02 19:47:48 -0400

    sent new data to vertex array buffer

--- WindowsOpenGLSandbox/Main.cpp
+++ WindowsOpenGLSandbox/Main.cpp
@@ -39,36 +39,42 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
 
   ShowWindow(vHwnd, nCmdShow);
  
-  //////////// openGL info
+  //////////// openGL info ////////////
+  
+  glEnable(GL_DEPTH_TEST);
+
   float vBufferData[] = 
   {
-     1.0F, 1.0F,       0.0F, 0.0F, 1.0F, 1.0F,
-    -1.0F, 1.0F,       0.0F, 1.0F, 0.0F, 1.0F,
+    -1.0F,   1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    -0.75F,  0.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    -0.5F,   1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
 
-     0.0F,   0.0F,     1.0F, 0.0F, 0.0F, 1.0,
-    -1.0F,  -1.0F,     0.0F, 1.0F, 0.0F, 1.0,
-     1.0F,  -1.0F,     0.0F, 0.0F, 1.0F, 1.0
+    -0.5F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    -0.25F, 0.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+     0.0F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+
+    0.0F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    0.25F, 0.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    0.5F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+
+    0.5F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    0.75F, 0.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
+    1.0F,  1.0F, 0.0F,      1.0F, 0.0F, 0.0F, 1.0F,
   };
 
-  GLuint vIndexBufferData[] = {0, 1, 2, 2, 3, 4};
-
   GLuint vVertexBufferId;
-  GLuint vIndexBufferId;
 
   glGenBuffers(1, &vVertexBufferId);
-  glGenBuffers(1, &vIndexBufferId);
 
   glBindBuffer(GL_ARRAY_BUFFER, vVertexBufferId);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vIndexBufferId);
 
   glBufferData(GL_ARRAY_BUFFER, sizeof(vBufferData), vBufferData, GL_STATIC_DRAW);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vIndexBufferData), vIndexBufferData, GL_STATIC_DRAW);
 
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
 
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 2));
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (void*)(sizeof(float) * 3));
 
   /////////////// end opengl info
 
@@ -89,8 +95,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
       DispatchMessage(&vMsg);
     }
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glClear(GL_DEPTH_BUFFER_BIT);
+
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 12);
 
     SwapBuffers(vDeviceContext);
   }
